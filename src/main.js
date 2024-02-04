9d10d4970434b0b95c2e7b9c5d9bbc55ef614ecf
DenisBahuta <denisboguto@gmail.com>
2024-02-04 18:17:53 +0100

    Add comments in code

--- src/main.js
+++ src/main.js
@@ -1,22 +1,32 @@
+// 1. Импортируются необходимые библиотеки и файлы. Здесь мы импортируем библиотеки axios, iziToast, SimpleLightbox и файлы стилей для iziToast и SimpleLightbox\.
+
 import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import icon from '../src/img/octagon.svg';
+import icon from '../src/img/icons.svg';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// 2. Получаются ссылки на элементы DOM. Мы получаем ссылки на форму поиска, список изображений, элемент загрузки и кнопку "Next";
 
 const formSearch = document.querySelector('.form');
 const imageList = document.querySelector('.gallery');
 const preload = document.querySelector('.loader');
 const nextBtn = document.querySelector('#next-btn');
 
+// 3. Устанавливаем начальное значение переменной `page` равным 0 и переменной `searchValue` равным `null`;
+
 let page = 0;
 let searchValue = null;
+
+// 4. Создаем экземпляр галереи SimpleLightbox, передавая ей селектор для изображений и объект с опциями;
 
 const gallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
+// 5. Определяем базовый URL для API и параметры поиска, используя объект URLSearchParams;
 
 const BASE_URL = 'https://pixabay.com/api';
 const searchParams = new URLSearchParams({
@@ -27,8 +37,13 @@ const searchParams = new URLSearchParams({
   per_page: 40,
 });
 
+// 6. Добавляем обработчики событий на форму поиска и кнопку "Next", вызывая соответствующие функции `handleSearch` и `nextPage`;
+
 formSearch.addEventListener('submit', handleSearch);
 nextBtn.addEventListener('click', nextPage);
+
+// 7. Создается функция обработки события отправки формы поиска.
+// Эта функция вызывается при отправке формы поиска. Она предотвращает стандартное поведение формы, получает значение поискового запроса из поля ввода, устанавливает начальные значения переменных `page` и `searchValue`, скрывает кнопку "Next", очищает список изображений и показывает элемент загрузки. Если поисковый запрос пустой, вызывается сообщение об ошибке с помощью библиотеки iziToast. Затем функция вызывает функцию `fetchImages`, чтобы получить изображения с API. Если ответ пустой, вызывается сообщение об ошибке. Если ответ не пустой, функция создает разметку галереи с помощью функции `createMarkup`, добавляет ее в список изображений, обновляет галерею SimpleLightbox, показывает кнопку "Next" и вызывает функцию `scrollBy`, чтобы прокрутить страницу к списку изображений. Наконец, функция скрывает элемент загрузки;
 
 async function handleSearch(event) {
   event.preventDefault();
@@ -45,8 +60,8 @@ async function handleSearch(event) {
       theme: 'light',
       message: `Please, fill in the search field`,
       messageSize: '20px',
-      messageColor: '#808080',
-      backgroundColor: '#e7fc44',
+      messageColor: '#FFFFFF',
+      backgroundColor: '#EF4040',
       position: 'topRight',
       timeout: 3000,
     });
@@ -85,6 +100,8 @@ async function handleSearch(event) {
   }
 }
 
+// 8. Создается функция для запроса изображений через API. Эта функция использует библиотеку axios для отправки GET\-запроса к API с параметрами поиска и номером страницы\. Если запрос выполнен успешно, функция возвращает данные ответа. Если произошла ошибка, функция выбрасывает исключение;
+
 async function fetchImages(value) {
   try {
     const res = await axios.get(
@@ -95,6 +112,8 @@ async function fetchImages(value) {
     throw new Error(err);
   }
 }
+
+// 9. Создается функция для создания разметки галереи. Эта функция принимает массив изображений и создает HTML-разметку галереи с помощью метода `map`. Затем функция объединяет элементы массива в строку с помощью метода `join` и возвращает эту строку;
 
 function createMarkup(arr) {
   return arr
@@ -133,6 +152,8 @@ function createMarkup(arr) {
     .join('');
 }
 
+// 10. Создается функция для обработки ошибок. Функция handleError выводит сообщение об ошибке в случае возникновения ошибки при выполнении запроса к API или при обработке данных. Также она удаляет обработчик события клика по кнопке "Next" и скрывает эту кнопку;
+
 function handleError(err) {
   console.error(err);
   imageList.innerHTML = '';
@@ -150,6 +171,7 @@ function handleError(err) {
   nextBtn.removeEventListener('click', nextPage);
   nextBtn.classList.add('is-hidden');
 }
+// 11. Функция nextPage увеличивает значение переменной page на 1 и вызывает функцию handleSearch для выполнения нового запроса к API с новой страницей. Если searchValue равно null, то функция завершается без выполнения запроса;
 
 async function nextPage() {
   preload.classList.remove('is-hidden');
@@ -195,6 +217,7 @@ async function nextPage() {
     preload.classList.add('is-hidden');
   }
 }
+// 12. Создается функция для прокрутки страницы к списку изображений. Эта функция использует метод `scrollBy` для прокрутки страницы к списку изображений с плавной анимацией;
 
 function scrollBy() {
   window.scrollBy({
